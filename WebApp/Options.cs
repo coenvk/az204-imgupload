17415cd5fa3ce0eafdc276097f4812ddbb23304f
OVSOFTWARE\CvanKampen <coen.van.kampen@ovsoftware.com>
2024-10-01 10:42:36 +0200

    Initial commit

---
+++ WebApp/Options.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp;
+
+public class Options
+{
+    public string ApiUrl { get; set; }
+}
---
+++ WebApp/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<Options>(builder.Configuration.Get<Options>());
+builder.Services.AddSingleton(new HttpClient());
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
